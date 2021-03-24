542e025bc4a3321ac31a9014d2019857dc8a549a
YASIN MOLLAH <yasinmollah96@gmail.com>
2021-03-24 22:11:36 +0600

    Initialization to the Project Titanic

---
+++ InitToProject.R
@@ -0,0 +1,27 @@
+#####################################
+#      Intro to ML Traning - R      #
+# Please come back for updated file #
+#####################################
+
+# Script 1 - Video 5
+
+# Set the working directory
+
+setwd("~/Learning/projectTitanic")
+
+# Import the training set: train
+# Your working directory might vary
+
+train <- read.csv("~/Learning/projectTitanic/train.csv")
+
+# viewing the "train" dataframe in raw format
+
+train
+
+# viewing the "train" dataframe in table format
+
+View(train)
+
+# dataframe structure
+
+str(train)
