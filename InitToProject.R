15314d8b863e1be386be5f30b21aa16f6f714260
YASIN MOLLAH <yasinmollah96@gmail.com>
2021-03-29 01:40:00 +0600

    Successfully submitted 4th prediction

--- InitToProject.R
+++ InitToProject.R
@@ -65,3 +65,24 @@ write.csv(prediction2,file = "2ndPrediction.csv",row.names = FALSE)
 
 
 #Starting the section for third prediction
+train$Fare2<-"30+"
+train$Fare2[train$Fare<30 & train$Fare>=20]<-"20-30"
+train$Fare2[train$Fare<20 & train$Fare>=10]<-"10-20"
+train$Fare2[train$Fare<10]<-"<10"
+View(train)
+table(train$Fare2)
+barplot(table(train$Fare2))
+
+aggregate(Survived~Fare2+Pclass+Sex,data=train,FUN = sum)
+
+aggregate(Survived~Fare2+Pclass+Sex,data=train,FUN = length)
+
+aggregate(Survived~Fare2+Pclass+Sex,data=train,FUN = function(x){sum(x)/length(x)})
+
+test$Survived<-0
+test$Survived[test$Sex=="female"]<-1
+test$Survived[test$Sex == "female" & test$Pclass==3 & test$Fare>=20]<-0
+
+#barplot(table(test$Survived))
+
+#Submission File ready for 3rd prediction
